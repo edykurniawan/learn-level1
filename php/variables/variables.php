e783167d3c0c523b36fd1f73008b9d2b2c9dec78
Ady Rahmat MA <ngurajeka.92@gmail.com>
2015-05-10 14:29:51 +0700

    variables in php and python

---
+++ php/variables/variables.php
@@ -0,0 +1,19 @@
+<?php
+// nama_variabel = (type data) nilai variabel
+$integer = (int) 10;
+// atau
+$integer = 10;
+
+$string = "IT'S STRING";
+// atau
+$string = (string) "IT\"S STRING";
+// atau
+$string = 'IT\'S STRING';
+
+$double = (double) 4.50;
+// atau
+$double = 4.50;
+
+$boolean = false;
+// atau
+$boolean = true;
---
+++ python/variables/variables.py
@@ -0,0 +1,17 @@
+# nama variabel = nilai variabel
+my_integer = 50
+
+my_string = "IT'S ME"
+# atau
+my_string = 'IT\'S ME'
+# atau
+my_string = """
+IT'S ME
+IT'S YOU
+"""
+
+my_double = 50.50
+
+my_boolean = True
+# atau
+my_boolean = False
