9d40240c11d5f474e6817d48ea951e4dc0203ded
Ady Rahmat MA <ngurajeka.92@gmail.com>
2015-05-10 15:54:11 +0700

    operator aritmatika dasar di php

---
+++ php/operators/operator-aritmatika.php
@@ -0,0 +1,19 @@
+<?php
+$angka1 = (int) 10;
+$angka2 = 50;
+
+// pertambahan
+echo $angka1 + $angka2;
+
+// pengurangan
+echo $angka1 - $angka2;
+
+// perkalian
+echo $angka1 * $angka2;
+
+// pembagi
+echo $angka1 / $angka2;
+
+// operator aritmatika dan assignment (memberikan nilai ke variabel lain)
+
+$hasil = $angka1 + $angka2;
