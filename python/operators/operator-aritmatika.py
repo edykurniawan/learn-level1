a8e97af97b0d1812139467b3c04a5138912726e1
edykurniawan <3dhiykurniawan@gmail.com>
2015-05-10 18:55:03 +0800

    Operator dan python

--- python/operators/operator-aritmatika.py
+++ python/operators/operator-aritmatika.py
@@ -1,1 +1,19 @@
 # memulai python dan operator arimatika
+
+angka1 = 10
+angka2 = 20
+
+# pertambahan
+print angka1 + angka2
+
+# pengurangan
+print angka1 - angka2
+
+# perkalian
+print angka1 * angka2
+
+# pembagian
+print angka1 / angka2
+
+#assigment
+hasil = angka1 + angka2
