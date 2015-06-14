00886bc85d96c1f8b26fe22af11122cbbe963e97
edykurniawan <3dhiykurniawan@gmail.com>
2015-06-14 19:01:22 +0800

    array assosiative dan multidimensi

---
+++ php/array/associative.php
@@ -0,0 +1,11 @@
+<?php
+
+$linux['mint'] = "Linux Mint";
+$linux['ubuntu'] = "Linux Ubuntu";
+$linux['kali'] = "Kali Linux";
+
+echo "OS yang saya pake sekarang adalah ". $linux['mint'] . "\n";
+echo "OS yang saya pake sekarang adalah ". $linux['ubuntu'] . "\n";
+echo "OS yang saya pake sekarang adalah ". $linux['kali'] . "\n";
+
+?>
---
+++ php/array/multidimensi.php
@@ -0,0 +1,20 @@
+<?php
+
+$sosmed = array(
+	'facebook'  => array(
+		'name' => 'facebook',
+		'url' => 'http://facebook.com',
+		),
+        'twitter'  => array(         
+       		'name' => 'Twitter',
+        	'url' => 'http://twitter.com',
+        	),
+        'gplus'  => array(         
+        	'name' => 'gplus',
+        	'url' => 'http://plus.google.com',
+       		),
+	);
+
+echo $sosmed['twitter']['url'];
+
+?>
